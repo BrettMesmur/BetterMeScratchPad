b1cd416446bc5d376f3b13dff209fb83ef10ef2c
BrettMesmur <76138443+BrettMesmur@users.noreply.github.com>
2025-11-04 08:57:05 -0700

    sp

---
+++ sw.js
@@ -0,0 +1,28 @@
+// sw.js
+const CACHE = "taps-v1";      // bump this on every deploy that changes cached files
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
